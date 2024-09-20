b4c4531062c4ece257e72d847e2df4e76f2b8290
beepityboopity <143839769+beepityboopity@users.noreply.github.com>
2024-09-20 15:54:23 -0400

    almost ready!

--- source/main.cpp
+++ source/main.cpp
@@ -6,23 +6,41 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <random>
+
+/* random int generation
+
+    std::random_device rd; // obtain a random number from hardware
+    std::mt19937 gen(rd()); // seed the generator
+    std::uniform_int_distribution<> distr(0, 10); // define the range
+
+    for(int n=0; n<40; ++n)
+        std::cout << distr(gen) << ' '; // generate numbers
+
+*/
+
 
 void frameCallback(GLFWwindow* window, int width, int height);
 void camInput(GLFWwindow* window);
 void moveActive(GLFWwindow* window);
 bool collisionTest();
-
+void newPieceSpawn();
+void gravity();
+void orderArray();
+void printCubeArray();
+void completionCheck();
 
 int rotationVar1 = 20;
 int rotationVar2 = 45;
 float zoomVar = -10;
 
-glm::vec3 cubePos = glm::vec3(-45, 5, 45);
 double lastMove = 0.0;
-
+double lastGrav = 0.0;
+int currentSpawn = 0;
 glm::vec3 temp[8];
 
 glm::vec4 cubeArray[1000] = {};
+glm::vec4 completionQueue[1000] = {};
 
 glm::vec3 octoCube[8] = {
 
@@ -39,14 +57,14 @@ glm::vec3(15, 15, 15),
 
 glm::vec3 octoLine[8] = {
 
-glm::vec3(0.05, 0.0, 0.05),
-glm::vec3(0.05, 0.05, 0.05),
-glm::vec3(0.05, 0.1, 0.05),
-glm::vec3(0.05, 0.15, 0.05),
-glm::vec3(0.05, 0.2, 0.05),
-glm::vec3(0.05, 0.25, 0.05),
-glm::vec3(0.05, 0.3, 0.05),
-glm::vec3(0.05, 0.35, 0.05),
+glm::vec3(5, 5, 5),
+glm::vec3(5, 15, 5),
+glm::vec3(5, 25, 5),
+glm::vec3(5, 35, 5),
+glm::vec3(5, 45, 5),
+glm::vec3(5, 55, 5),
+glm::vec3(5, 65, 5),
+glm::vec3(5, 75, 5),
 
 };
 
@@ -65,13 +83,37 @@ glm::vec3(25, 35, 5),
 
 glm::vec3 activePiece[8];
 
+
+glm::vec3 spawns[6] {
+
+    glm::vec3(0, 10, 0),  // bottom
+    glm::vec3(0, -10, 0), // top
+    glm::vec3(10, 0, 0),  // front right
+    glm::vec3(-10, 0, 0), // front left
+    glm::vec3(0, 0, 10),  // back right
+    glm::vec3(0, 0, -10), // back left
+
+};
+
+std::random_device rd;
+std::mt19937 gen(rd());
+std::uniform_int_distribution<> distr(0, 5);
+
 int main() {
     for(int i = 0; i < 8; i++) activePiece[i] = octoCube[i];
     for(int i = 0; i < 8; i++) activePiece[i].y += 100;
 
-    for(int i = 0; i < 1000; i++) cubeArray[i] = glm::vec4(30, 30, 30, -1.0);
+    for(int i = 0; i < 1000; i++) cubeArray[i] = glm::vec4(-1, -1, -1, -1.0);
 
-    for(int i = 0; i < 8; i++) cubeArray[i] = glm::vec4(octoI[i], 1.0);
+    for(int i = -45; i < 30; i += 10) {
+        cubeArray[i + 45] = glm::vec4(i, -45, 45, 1.0);
+    }
+
+    for(int i = -45; i < 30; i += 10) {
+        cubeArray[i + 45 + 1] = glm::vec4(i, -25, 45, 1.0);
+    }
+
+    orderArray();
 
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -202,6 +244,7 @@ int main() {
 
         camInput(window);
         moveActive(window);
+        gravity();
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -223,15 +266,7 @@ int main() {
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
-        // cube
-        model = glm::mat4(1.0f);
-        model = glm::translate(model, glm::vec3((float)cubePos.x / 100, (float)cubePos.y / 100, (float)cubePos.z / 100));
-        model = glm::scale(model, glm::vec3(0.1, 0.1, 0.1));
         baseShader.setInt("currentTexture", 1);
-        baseShader.setMat4("model", model);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-
-
         // shape test
         for(int i = 0; i < 8; i++) {
             model = glm::mat4(1.0f);
@@ -267,11 +302,11 @@ void frameCallback(GLFWwindow* window, int width, int height) {
 }
 
 void camInput(GLFWwindow* window) {
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) rotationVar1 += 1;
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) rotationVar1 -= 1;
+    if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS) rotationVar1 += 1;
+    if (glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS) rotationVar1 -= 1;
 
-    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) rotationVar2 += 1;
-    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) rotationVar2 -= 1;
+    if (glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS) rotationVar2 += 1;
+    if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS) rotationVar2 -= 1;
 
     if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS) zoomVar -= 0.5;
     if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS) zoomVar += 0.5;
@@ -288,34 +323,52 @@ void moveActive(GLFWwindow* window) {
 
     for(int i = 0; i < 8; i++) temp[i] = activePiece[i];
 
-    if (glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS) {
-        for(int i = 0; i < 8; i++) {
+    if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+        if(spawns[currentSpawn].y != 0) {
+            for(int i = 0; i < 8; i++) activePiece[i].x += 10;
 
-            if(activePiece[i].x >= 40) {
-                return;
-            }
         }
-        for(int i = 0; i < 8; i++) activePiece[i].x += 10;
+        else {
+            for(int i = 0; i < 8; i++) activePiece[i].y += 10;
+        }
         lastMove = glfwGetTime();
     }
-    if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS) {
-        for(int i = 0; i < 8; i++) activePiece[i].x -= 10;
+
+    if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+        if(spawns[currentSpawn].y != 0) {
+            for(int i = 0; i < 8; i++) activePiece[i].x += -10;
+
+        }
+        else {
+            for(int i = 0; i < 8; i++) activePiece[i].y += -10;
+        }
         lastMove = glfwGetTime();
     }
-    if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS) {
-        for(int i = 0; i < 8; i++) activePiece[i].z -= 10;
+
+    if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+        if(spawns[currentSpawn].z != 0) {
+            for(int i = 0; i < 8; i++) activePiece[i].x += -10;
+
+        }
+        else {
+            for(int i = 0; i < 8; i++) activePiece[i].z += -10;
+        }
         lastMove = glfwGetTime();
     }
-    if (glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS) {
-        for(int i = 0; i < 8; i++) activePiece[i].z += 10;
+
+    if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+        if(spawns[currentSpawn].z != 0) {
+            for(int i = 0; i < 8; i++) activePiece[i].x += 10;
+
+        }
+        else {
+            for(int i = 0; i < 8; i++) activePiece[i].z += 10;
+        }
         lastMove = glfwGetTime();
     }
-    if (glfwGetKey(window, GLFW_KEY_U) == GLFW_PRESS) {
-        for(int i = 0; i < 8; i++) activePiece[i].y -= 10;
-        lastMove = glfwGetTime();
-    }
-    if (glfwGetKey(window, GLFW_KEY_O) == GLFW_PRESS) {
-        for(int i = 0; i < 8; i++) activePiece[i].y += 10;
+
+    if (glfwGetKey(window, GLFW_KEY_M) == GLFW_PRESS) {
+        newPieceSpawn();
         lastMove = glfwGetTime();
     }
     if(!collisionTest()) {
@@ -325,6 +378,24 @@ void moveActive(GLFWwindow* window) {
 
 bool collisionTest() {
     for(int i = 0; i < 8; i++) {
+
+        if(spawns[currentSpawn].x != 0) {
+            if((activePiece[i].y > 45 || activePiece[i].y < -45) || (activePiece[i].z > 45 || activePiece[i].z < -45)) return false;
+        }
+        else if(spawns[currentSpawn].y != 0) {
+            if((activePiece[i].x > 45 || activePiece[i].x < -45) || (activePiece[i].z > 45 || activePiece[i].z < -45)) return false;
+        }
+        else{
+            if((activePiece[i].y > 45 || activePiece[i].y < -45) || (activePiece[i].x > 45 || activePiece[i].x < -45)) return false;
+        }
+
+        if(spawns[currentSpawn].x == 10 && activePiece[i].x < -45) return false;
+        else if(spawns[currentSpawn].x == -10 && activePiece[i].x > 45) return false;
+        else if(spawns[currentSpawn].y == 10 && activePiece[i].y < -45) return false;
+        else if(spawns[currentSpawn].y == -10 && activePiece[i].y > 45) return false;
+        else if(spawns[currentSpawn].z == 10 && activePiece[i].z < -45) return false;
+        else if(spawns[currentSpawn].z == -10 && activePiece[i].z > 45) return false;
+
         int j = 0;
         while(cubeArray[j].w != -1.0) {
             if(glm::vec3(cubeArray[j].x, cubeArray[j].y, cubeArray[j].z) == activePiece[i]) {
@@ -335,4 +406,54 @@ bool collisionTest() {
     }
     return true;
 }
+
+void newPieceSpawn() {
+
+    currentSpawn = distr(gen);
+    int endInd = 0;
+    while(cubeArray[endInd].w != -1.0) endInd += 1;
+    for(int i = 0; i < 8; i++) cubeArray[endInd + i] = glm::vec4(activePiece[i].x, activePiece[i].y, activePiece[i].z, 1.0);
+    for(int i = 0; i < 8; i++) activePiece[i] = octoCube[i] + (spawns[currentSpawn] * glm::vec3(10));
+ }
+
+void gravity() {
+    if(glfwGetTime() - lastGrav < 1.0) return;
+    for(int i = 0; i < 8; i++) temp[i] = activePiece[i];
+    for(int i = 0; i < 8; i++) {
+        activePiece[i] -= spawns[currentSpawn];
+    }
+    if(!collisionTest()) {
+        for(int i = 0; i < 8; i++) activePiece[i] = temp[i];
+        newPieceSpawn();
+    }
+    lastGrav = glfwGetTime();
+}
+
+void orderArray() {
+    int endInd = 0;
+    while(cubeArray[endInd].x != -1) endInd++;
+    for(int i = endInd; i < 1000; i++) {
+        if(cubeArray[i].x != -1) {
+            cubeArray[endInd] = cubeArray[i];
+            cubeArray[i] = glm::vec4(-1, -1, -1, -1.0);
+            endInd++;
+        }
+    }
+}
+
+void printCubeArray() {
+    std::cout << "printing cubes array\n";
+    for(int i = 0; i < 1000; i += 4) std::cout << "(" << cubeArray[i].x << ", " << cubeArray[i].y << ", " << cubeArray[i].z << ")" << "(" << cubeArray[i + 1].x << ", "  << cubeArray[i + 1].y << ", " << cubeArray[i + 1].z << ")"<< "(" << cubeArray[i + 2].x << ", " << cubeArray[i + 2].y << ", " << cubeArray[i + 2].z << ")" << "(" << cubeArray[i + 3].x << ", " << cubeArray[i + 3].y << ", " << cubeArray[i + 3].z << ")\n";
+
+}
+
+void completionCheck() {
+
+}
+
+
+
+
+
+
 
